a1dec11739466ec07def8a6c288aed2d2a77ef75
yelikang <530331059@qq.com>
2024-06-18 10:54:44 +0800

    表格绘制

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="app">
+        <ul>
+            <li>
+                <a href="#/table">表格</a>
+            </li>
+        </ul>
+
+        <div>
+            <router-view></router-view>
+        </div>
+    </div>
+</template>
+<script>
+export default {}
+</script>
+
+<style lang="less" scoped>
+.app {
+    height: 100%;
+    display: flex;
+    ul {
+        flex-basis: 100px;
+        li {
+            list-style: none;
+            cursor: pointer;
+        }
+    }
+    > div {
+        flex-grow: 1;
+    }
+}
+</style>
---
+++ src/main.ts
@@ -0,0 +1,14 @@
+import Vue from 'vue'
+import App from './App.vue'
+import antd from 'ant-design-vue'
+import 'ant-design-vue/dist/antd.css'
+import router from './router'
+
+
+
+Vue.use(antd)
+Vue.config.devtools = true
+new Vue({
+    router,
+    render: (h) => h(App)
+}).$mount('#app')
